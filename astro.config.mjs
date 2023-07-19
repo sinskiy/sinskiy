26109fdd52e42d0c8065cff42c2d84c1aa9ec153
Sinskiy <sinskiy@proton.me>
2023-07-19 16:21:33 +0300

    fix deployment 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sinskiy.github.io',
-    base: '/Sinskiy',
+    base: '/',
 });
